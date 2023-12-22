fd4ad7211087d94c0a5ad8cbf8b3fe617e91646f
moonlaughs <iza8848@gmail.com>
2023-12-22 23:32:56 +0000

    Santa's behaviour

---
+++ Assets/ChaseScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChaseScript : StateMachineBehaviour
+{
+    NavMeshAgent agent;
+    Transform player;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent = animator.GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent.SetDestination(player.position);
+        float distance = Vector3.Distance(animator.transform.position, player.position);
+
+        if(distance < 3){
+            // Application.Quit(); // works in game build
+            UnityEditor.EditorApplication.isPlaying = false; // works in Unity Engine only
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       agent.SetDestination(agent.transform.position);
+    }
+}
